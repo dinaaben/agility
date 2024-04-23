53776e311f99443ef92fc6f3387b5281b409e7dc
dinaaben <46792559+Dinaben@users.noreply.github.com>
2024-04-23 16:09:59 -0400

    change config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,21 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'AGILITY',
+  tagline: 'DAGILITY is cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://dinaaben.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  trailingSlash: false,
+  baseUrl: '/agility/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dinaaben', // Usually your GitHub org/user name.
+  projectName: 'agility', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -58,7 +59,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'AGILITY Docs site',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
