7ced175b15ac1ea25419b9f999d2f690f2282f95
dinaaben <46792559+Dinaben@users.noreply.github.com>
2024-04-24 16:07:59 -0400

    deleted type

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
           label: 'Tutorial',
         },
         {
-          type: 'doc',
+          //type: 'doc',
           sidebarId: 'api',
           position: 'left',
           label: 'API',
