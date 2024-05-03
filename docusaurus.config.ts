5e924be34e2b4d9877dcfd2af2d9252d8d7a4d45
dinaaben <46792559+Dinaben@users.noreply.github.com>
2024-05-03 15:10:24 -0400

    change header name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Use AGILITY',
         },
         {
           type: 'docSidebar',
